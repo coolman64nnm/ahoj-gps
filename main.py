67f3d62be0e99b4495e552cbae66ad60e006dbe1
coolman64nnm <140909743+coolman64nnm@users.noreply.github.com>
2025-11-20 19:12:01 +0100

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 serial.redirect(SerialPin.P14, SerialPin.P12, BaudRate.BAUD_RATE115200)
 
 def on_forever():
+    basic.show_number(27)
     pass
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 serial.redirect(SerialPin.P14, SerialPin.P12, BaudRate.BaudRate115200)
 basic.forever(function on_forever() {
+    basic.showNumber(27)
     
 })
